e0f1373af1e82113a8c450e8705254821cfa3135
Iwo Błądek <iwo.bladek@cs.put.poznan.pl>
2020-11-17 17:56:41 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,6 @@
+from distutils.core import setup
+
+setup(name='pysv', version='0.3.0', packages=['pysv'])
+
+# Run the line below to install the package:
+# python3 -m pip install -e <absolute path to the directory containing this path>
